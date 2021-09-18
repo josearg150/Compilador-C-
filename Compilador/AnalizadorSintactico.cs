bd88ae1edf3769ab2529b7979931dd09a275cb43
Jose angel <73683594+josearg150@users.noreply.github.com>
2021-09-17 21:27:47 -0500

    Creada clase para analizador sintactico

---
+++ Compilador/AnalizadorSintactico.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Compilador
+{
+    /// <summary>
+    ///     Clase Analizador sintactico
+    ///     Contiene los metodos para analizar el codigo fuente 
+    /// </summary>
+    /// 
+    /// <Para>
+    ///   
+    /// </Para>
+    /// 
+    /// <Supuestos>
+    /// </Supuestos>
+    /// 
+    /// <Autor>
+    ///     Jose angel rocha garcia 
+    ///     Jose luis carreon reyes
+    /// </Autor>
+    /// 
+    /// <FechaCreacion >
+    ///     17/09/2021
+    /// </FechaCreacion>
+    class AnalizadorSintactico
+    {
+        //***************************************
+        //Variables locales                     
+        //***************************************
+        #region Variables
+        #endregion
+        //***************************************
+        //Constructores   
+        //***************************************
+        #region Constructores 
+        #endregion
+        //***************************************
+        //Metodos
+        //***************************************
+        #region Metodos
+        #endregion
+    }
+}
