ec7396632536e4e4081b4bdbbab9f849c78744a0
Jose angel <73683594+josearg150@users.noreply.github.com>
2021-09-17 21:45:42 -0500

    no me acuerdo que puse

--- Compilador/AnalizadorSintactico.cs
+++ Compilador/AnalizadorSintactico.cs
@@ -42,6 +42,10 @@ namespace Compilador
         //Metodos
         //***************************************
         #region Metodos
+        public void analizar()
+        {
+
+        }
         #endregion
     }
 }
