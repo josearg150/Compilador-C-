32c201e86d130638781cf902953b927860ecf8d1
cattybog <72874739+cattybog@users.noreply.github.com>
2021-09-22 19:41:02 -0500

    Clase arbol y nodo agregadas

---
+++ Compilador/Arbol.cs
@@ -0,0 +1,44 @@
+﻿namespace Compilador
+{
+    /// <summary>
+    ///     Clase arbol para generar un arbol binario 
+    ///     Contiene el nodo raiz
+    /// </summary>
+    /// 
+    /// <Para>
+    ///  Esta clase unicamente sirve para crear el arbol y sus nodos 
+    /// </Para>
+    /// 
+    /// <Supuestos>
+    ///     
+    /// </Supuestos>
+    /// 
+    /// <Autor>
+    ///     Jose angel rocha garcia 
+    ///     Jose luis carreon reyes
+    /// </Autor>
+    /// 
+    /// <FechaCreacion>
+    ///     04/09/2021
+    /// </FechaCreacion>
+    class Arbol
+    {
+        //***************************************
+        //Variables locales                     
+        //***************************************
+        #region Variables  
+        //con esta forma se crea la variable y sus metodos get y set 
+        public Nodo Raiz { set; get; }
+        #endregion
+
+        //***************************************
+        //Constructores   
+        //***************************************
+        #region Constructores
+        public Arbol(Nodo Raiz)
+        {
+            this.Raiz = Raiz;
+        }
+        #endregion Constructores
+    }
+}
---
+++ Compilador/Nodo.cs
@@ -0,0 +1,53 @@
+﻿namespace Compilador
+{
+    /// <summary>
+    ///     Clase nodo para la clase Arbol binario 
+    ///     Contiene los atributos de un Nodo 
+    /// </summary>
+    /// 
+    /// <Para>
+    ///     Con esta clase se pueden crear los nodos que seran usados por el arbol binario 
+    /// </Para>
+    /// 
+    /// <Supuestos>
+    /// </Supuestos>
+    /// 
+    /// <Autor>
+    ///     Jose angel rocha garcia 
+    ///     Jose luis carreon reyes
+    /// </Autor>
+    /// 
+    /// <FechaCreacion >
+    ///     30/08/2021
+    /// </FechaCreacion>
+    class Nodo
+    {   //***************************************
+        //Variables locales                     
+        //***************************************
+        #region Variables 
+        //De esta forma creamos la variable y al mismo tiempo sus metodos set and get 
+        public string Valor { set; get; }
+        public Nodo Izquierda { set; get; }
+        public Nodo Derecha { set; get; }
+        #endregion
+        //***************************************
+        //Constructores   
+        //***************************************
+        #region Constructores 
+        //Con parametros 
+        public Nodo(string Valor)
+        {
+            this.Valor = Valor;
+            this.Izquierda = null;
+            this.Derecha = null;
+        }
+        //vacio
+        public Nodo()
+        {
+            this.Izquierda = null;
+            this.Derecha = null;
+        }
+        #endregion 
+
+    }
+}
